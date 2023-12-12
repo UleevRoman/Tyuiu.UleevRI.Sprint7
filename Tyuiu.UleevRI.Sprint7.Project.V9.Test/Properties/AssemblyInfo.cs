b7661bf8deb231e38dffebd37729c3d827f5f670
Tyuiu.UleevRI@outlook.com <romanuleev178@gmail.com>
2023-12-13 00:53:11 +0500

    Add project files.

---
+++ Tyuiu.UleevRI.Sprint7.Project.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.UleevRI.Sprint7.Project.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.UleevRI.Sprint7.Project.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3502eada-34b9-4c20-a4a0-deaa39bf02a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.UleevRI.Sprint7.Project.V9/FormMain.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Tyuiu.UleevRI.Sprint7.Project.V9
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.panelHelp_URI = new System.Windows.Forms.Panel();
+            this.buttonHelp_URI = new System.Windows.Forms.Button();
+            this.panelMenu_URI = new System.Windows.Forms.Panel();
+            this.splitterMenu_URI = new System.Windows.Forms.Splitter();
+            this.buttonOpenFile_URI = new System.Windows.Forms.Button();
+            this.buttonSaveFile_URI = new System.Windows.Forms.Button();
+            this.buttonBack_URI = new System.Windows.Forms.Button();
+            this.buttonFile_URI = new System.Windows.Forms.Button();
+            this.button = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.panelActions_URI = new System.Windows.Forms.Panel();
+            this.panelHelp_URI.SuspendLayout();
+            this.panelMenu_URI.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelHelp_URI
+            // 
+            this.panelHelp_URI.BackColor = System.Drawing.Color.White;
+            this.panelHelp_URI.Controls.Add(this.button);
+            this.panelHelp_URI.Controls.Add(this.buttonFile_URI);
+            this.panelHelp_URI.Controls.Add(this.buttonBack_URI);
+            this.panelHelp_URI.Controls.Add(this.buttonHelp_URI);
+            this.panelHelp_URI.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHelp_URI.Location = new System.Drawing.Point(0, 0);
+            this.panelHelp_URI.Name = "panelHelp_URI";
+            this.panelHelp_URI.Size = new System.Drawing.Size(1219, 59);
+            this.panelHelp_URI.TabIndex = 0;
+            // 
+            // buttonHelp_URI
+            // 
+            this.buttonHelp_URI.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonHelp_URI.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonHelp_URI.Image = ((System.Drawing.Image)(resources.GetObject("buttonHelp_URI.Image")));
+            this.buttonHelp_URI.Location = new System.Drawing.Point(1133, 0);
+            this.buttonHelp_URI.Name = "buttonHelp_URI";
+            this.buttonHelp_URI.Size = new System.Drawing.Size(86, 59);
+            this.buttonHelp_URI.TabIndex = 0;
+            this.buttonHelp_URI.UseVisualStyleBackColor = true;
+            // 
+            // panelMenu_URI
+            // 
+            this.panelMenu_URI.BackColor = System.Drawing.Color.SeaShell;
+            this.panelMenu_URI.Controls.Add(this.buttonOpenFile_URI);
+            this.panelMenu_URI.Controls.Add(this.buttonSaveFile_URI);
+            this.panelMenu_URI.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu_URI.Location = new System.Drawing.Point(0, 59);
+            this.panelMenu_URI.Name = "panelMenu_URI";
+            this.panelMenu_URI.Size = new System.Drawing.Size(76, 722);
+            this.panelMenu_URI.TabIndex = 1;
+            // 
+            // splitterMenu_URI
+            // 
+            this.splitterMenu_URI.Location = new System.Drawing.Point(76, 59);
+            this.splitterMenu_URI.Name = "splitterMenu_URI";
+            this.splitterMenu_URI.Size = new System.Drawing.Size(10, 722);
+            this.splitterMenu_URI.TabIndex = 2;
+            this.splitterMenu_URI.TabStop = false;
+            // 
+            // buttonOpenFile_URI
+            // 
+            this.buttonOpenFile_URI.BackColor = System.Drawing.Color.Red;
+            this.buttonOpenFile_URI.Image = ((System.Drawing.Image)(resources.GetObject("buttonOpenFile_URI.Image")));
+            this.buttonOpenFile_URI.Location = new System.Drawing.Point(6, 6);
+            this.buttonOpenFile_URI.Name = "buttonOpenFile_URI";
+            this.buttonOpenFile_URI.Size = new System.Drawing.Size(64, 54);
+            this.buttonOpenFile_URI.TabIndex = 1;
+            this.buttonOpenFile_URI.UseVisualStyleBackColor = false;
+            // 
+            // buttonSaveFile_URI
+            // 
+            this.buttonSaveFile_URI.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.buttonSaveFile_URI.Image = ((System.Drawing.Image)(resources.GetObject("buttonSaveFile_URI.Image")));
+            this.buttonSaveFile_URI.Location = new System.Drawing.Point(6, 66);
+            this.buttonSaveFile_URI.Name = "buttonSaveFile_URI";
+            this.buttonSaveFile_URI.Size = new System.Drawing.Size(64, 54);
+            this.buttonSaveFile_URI.TabIndex = 2;
+            this.buttonSaveFile_URI.UseVisualStyleBackColor = false;
+            // 
+            // buttonBack_URI
+            // 
+            this.buttonBack_URI.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonBack_URI.Image = ((System.Drawing.Image)(resources.GetObject("buttonBack_URI.Image")));
+            this.buttonBack_URI.Location = new System.Drawing.Point(12, 12);
+            this.buttonBack_URI.Name = "buttonBack_URI";
+            this.buttonBack_URI.Size = new System.Drawing.Size(45, 34);
+            this.buttonBack_URI.TabIndex = 3;
+            this.buttonBack_URI.UseVisualStyleBackColor = true;
+            // 
+            // buttonFile_URI
+            // 
+            this.buttonFile_URI.BackColor = System.Drawing.Color.White;
+            this.buttonFile_URI.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFile_URI.Location = new System.Drawing.Point(84, 12);
+            this.buttonFile_URI.Name = "buttonFile_URI";
+            this.buttonFile_URI.Size = new System.Drawing.Size(75, 33);
+            this.buttonFile_URI.TabIndex = 4;
+            this.buttonFile_URI.Text = "файл";
+            this.buttonFile_URI.UseVisualStyleBackColor = false;
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button.Location = new System.Drawing.Point(174, 12);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(117, 33);
+            this.button.TabIndex = 5;
+            this.button.Text = "графики";
+            this.button.UseVisualStyleBackColor = true;
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel.Location = new System.Drawing.Point(86, 736);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1133, 45);
+            this.panel.TabIndex = 3;
+            // 
+            // panelActions_URI
+            // 
+            this.panelActions_URI.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panelActions_URI.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelActions_URI.Location = new System.Drawing.Point(86, 59);
+            this.panelActions_URI.Name = "panelActions_URI";
+            this.panelActions_URI.Size = new System.Drawing.Size(1133, 677);
+            this.panelActions_URI.TabIndex = 4;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1219, 781);
+            this.Controls.Add(this.panelActions_URI);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.splitterMenu_URI);
+            this.Controls.Add(this.panelMenu_URI);
+            this.Controls.Add(this.panelHelp_URI);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panelHelp_URI.ResumeLayout(false);
+            this.panelMenu_URI.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHelp_URI;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button buttonFile_URI;
+        private System.Windows.Forms.Button buttonBack_URI;
+        private System.Windows.Forms.Button buttonHelp_URI;
+        private System.Windows.Forms.Panel panelMenu_URI;
+        private System.Windows.Forms.Button buttonOpenFile_URI;
+        private System.Windows.Forms.Button buttonSaveFile_URI;
+        private System.Windows.Forms.Splitter splitterMenu_URI;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Panel panelActions_URI;
+    }
+}
+
