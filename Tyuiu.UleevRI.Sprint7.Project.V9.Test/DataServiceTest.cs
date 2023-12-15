4a29b8269ad65f029b2ce7a4392a3aeb4a0fe8a7
Tyuiu.UleevRI@outlook.com <romanuleev178@gmail.com>
2023-12-15 22:03:22 +0500

    Add project files.

--- Tyuiu.UleevRI.Sprint7.Project.V9.Test/DataServiceTest.cs
+++ Tyuiu.UleevRI.Sprint7.Project.V9.Test/DataServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 using Tyuiu.UleevRI.Sprint7.Project.V9.Lib;
 
 namespace Tyuiu.UleevRI.Sprint7.Project.V9.Test
@@ -8,8 +9,12 @@ namespace Tyuiu.UleevRI.Sprint7.Project.V9.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LoadFromDataFile()
         {
+            string path = @"C:\DataSprint7\БД Видеолента.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
         }
     }
 }
